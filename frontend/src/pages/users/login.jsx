13cf342d866b2ffc3aa85be2e8f749b0d2e10aa4
Dakar13 <ecalvamtz@gmail.com>
2020-05-15 16:06:33 -0500

    Fogg-UI instalados y Pagina de Login renderizada funcionando

---
+++ frontend/src/pages/users/login.jsx
@@ -0,0 +1,7 @@
+// Components
+import LoginLayout from '../../app/users/components/Login/Layout'
+// import LoginLayout from '../../../src/app/users/components/Login/Layout'
+
+const LoginPage = () => <LoginLayout />
+
+export default LoginPage
