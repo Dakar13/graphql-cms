97b3cd363adac1ab0cb20314d7f9fef5531eec6c
Dakar13 <ecalvamtz@gmail.com>
2020-05-15 17:36:03 -0500

    Archivos de configuracion frontent generados y funcionando

--- frontend/src/pages/users/login.jsx
+++ frontend/src/pages/users/login.jsx
@@ -1,6 +1,5 @@
 // Components
 import LoginLayout from '../../app/users/components/Login/Layout'
-// import LoginLayout from '../../../src/app/users/components/Login/Layout'
 
 const LoginPage = () => <LoginLayout />
 
